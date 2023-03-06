b8adf29e6d99802a2934892102eb9493937ba73c
sjgeddam <sgeddam@utexas.edu>
2023-03-06 15:09:50 -0600

    Add UI views for desired input & add set up logic

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,34 +10,34 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
-    
+    @State private var coffeAmount = 1
+
     var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                .labelsHidden()
-            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-            Text(Date.now, format: .dateTime.hour().minute())
-            Text(Date.now.formatted(date: .long, time: .omitted))
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeAmount == 1 ? "1 cup" : "\(coffeAmount) cups", value: $coffeAmount, in: 1...20, step: 1)
+            }
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button("Calculate", action: calcBedtime)
+            }
         }
-        .padding()
     }
     
-    func trivExample() {
-//        var components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? Date.now
+    func calcBedtime() {
         
-        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
-        
-    }
-    
-    func exampleDate() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
     }
 }
 
