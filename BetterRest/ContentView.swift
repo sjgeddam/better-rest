c0ca20ed2a07172e652b8781104fbecf77993dc9
sjgeddam <sgeddam@utexas.edu>
2023-03-05 19:24:12 -0600

    Add examples of Stepper and DatePicker

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                .labelsHidden()
+            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
         }
         .padding()
+    }
+    
+    func exampleDate() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tomorrow
     }
 }
 
