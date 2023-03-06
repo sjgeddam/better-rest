691d91bdb4fa05c8bf11bc49076b94a6bf9eb4b3
sjgeddam <sgeddam@utexas.edu>
2023-03-05 19:37:38 -0600

    Add demo code for Date

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -17,8 +17,22 @@ struct ContentView: View {
             DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
                 .labelsHidden()
             DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+            Text(Date.now, format: .dateTime.hour().minute())
+            Text(Date.now.formatted(date: .long, time: .omitted))
         }
         .padding()
+    }
+    
+    func trivExample() {
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? Date.now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        
     }
     
     func exampleDate() {
