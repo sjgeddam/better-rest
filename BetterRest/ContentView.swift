7343ddf7748621cfff9cacae0d1d194f6c0079e6
sjgeddam <sgeddam@utexas.edu>
2023-03-06 22:37:51 -0600

    Add logic to calcBedtime function & show alert based on result

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,12 +5,17 @@
 //  Created by Ram G on 3/5/23.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var coffeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
 
     var body: some View {
         NavigationView {
@@ -33,10 +38,38 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calcBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
     func calcBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            // more code to come
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            // something went wrong
+            alertTitle = "Error"
+            alertMessage = "Sorry there was a problem calculating your bed time"
+        }
+        
+        showingAlert = true
         
     }
 }
